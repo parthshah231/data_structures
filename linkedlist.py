c3cfec46c5fccc553b86d1cfe42da2d4f560e742
Parth <parthshah231@gmail.com>
2023-08-11 19:28:41 -0300

    add append, remove methods

--- linkedlist.py
+++ linkedlist.py
@@ -12,6 +12,40 @@ class LinkedList:
     def __init__(self) -> None:
         self.head: Optional[Node] = None
 
+    def append(self, value: int):
+        new: Node = Node(value=value)
+        if self.head is None:
+            self.head = new
+            return
+
+        node = self.head
+        while node.next is not None:
+            node = node.next
+        node.next = new
+
+    def remove(self, idx: int):
+        if self.head is None:
+            return
+
+        if idx == 0:
+            self.head = self.head.next
+            return
+
+        node: Node = self.head
+        i = 0
+
+        while node.next is not None and i < idx - 1:
+            node = node.next
+            i += 1
+
+        if node.next is None:
+            raise ValueError(f"Index {idx} is out of bounds of the linked list")
+
+        if node.next.next is not None:
+            node.next = node.next.next
+        else:
+            node.next = None
+
     def __str__(self) -> str:
         node = self.head
         if node is None:
@@ -28,4 +62,10 @@ class LinkedList:
 
 if __name__ == "__main__":
     ll = LinkedList()
+    ll.append(value=1)
+    ll.append(value=2)
+    ll.append(value=3)
+    ll.remove(idx=1)
+    ll.append(value=4)
+    ll.remove(idx=1)
     print(ll)
