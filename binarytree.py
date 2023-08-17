99d462b25260a252b3cd94b699df86a4eb7f703c
Parth <parthshah231@gmail.com>
2023-08-17 08:09:40 -0300

    add methods findMinValue and mergeTrees

--- binarytree.py
+++ binarytree.py
@@ -1,85 +1,81 @@
 from __future__ import annotations
-from typing import Optional, Literal
+from typing import List, Optional
+
 from collections import deque
 
 
 class TreeNode:
-    def __init__(self, value: int = 0) -> None:
-        self.value = value
-        self.left: Optional[TreeNode] = None
-        self.right: Optional[TreeNode] = None
+    def __init__(self, val: int) -> None:
+        self.val = val
+        self.left = None
+        self.right = None
 
 
-def balance_height(root: TreeNode):
+def insert():
     pass
 
 
-def is_balanced(root: TreeNode) -> bool:
-    pass
+def findMinValue(root):
+    if root is None:
+        return
+
+    leftMin = findMinValue(root.left) if root.left is not None else float("inf")
+    rightMin = findMinValue(root.right) if root.right is not None else float("inf")
+    return min(root.val, leftMin, rightMin)
 
 
-def isSymmetric_helper(left: Optional[TreeNode], right: Optional[TreeNode]):
-    pass
+def mergeTrees(root1, root2):
+    if root1 is None and root2 is None:
+        return
+
+    if root1 is None:
+        return root2
+
+    if root2 is None:
+        return root1
+
+    s = TreeNode(root1.val + root2.val)
+    s.left = mergeTrees(root1.left, root2.left)
+    s.right = mergeTrees(root1.right, root2.right)
+
+    return s
 
 
-def isSymmetric(root: Optional[TreeNode]) -> bool:
-    pass
+def display(root, traversal_type="bfs"):
+    if root is None:
+        print("Empty tree.")
+        return
 
-
-def treeSum(root: TreeNode):
-    pass
-
-
-def display(
-    root: TreeNode,
-    traversal_type: Literal["in-order", "post-order", "dfs", "bfs"] = "in-order",
-):
-    if root:
-        if traversal_type == "in-order":
-            display(root.left, traversal_type)
-            print(root.value)
-            display(root.right, traversal_type)
-        elif traversal_type == "post-order":
-            display(root.left, traversal_type)
-            display(root.right, traversal_type)
-            print(root.value)
-        elif traversal_type == "dfs":
-            stack = [root]
-            while stack:
-                curr = stack.pop()
-                print(curr.value)
-                if curr.left:
-                    stack.append(curr.left)
-                if curr.right:
-                    stack.append(curr.right)
-        elif traversal_type == "bfs":
-            queue = deque([root])
-            while queue:
-                curr = queue.popleft()
-                print(curr.value)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-
-        else:
-            raise ValueError(f"Traversal Type:{traversal_type} not implemented")
+    if traversal_type == "bfs":
+        queue = deque([root])
+        while queue:
+            curr = queue.popleft()
+            print(curr.val)
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+    else:
+        raise ValueError(f"{traversal_type} not implemented.")
 
 
 if __name__ == "__main__":
-    root = TreeNode(5)
-    node1 = TreeNode(2)
+    root = TreeNode(-1)
+
+    node1 = TreeNode(1)
     node2 = TreeNode(2)
     node3 = TreeNode(3)
+
     node4 = TreeNode(4)
-    node5 = TreeNode(4)
-    node6 = TreeNode(3)
+    node5 = TreeNode(5)
+    node6 = TreeNode(6)
 
-    root.left = node1
-    root.right = node2
-    node1.left = node3
-    node1.right = node4
-    node2.left = node5
-    node2.right = node6
+    node1.left = node2
+    node1.right = node3
 
-    display(root, traversal_type="bfs")
+    node4.left = node5
+    node4.right = node6
+
+    # print(findMinValue(root))
+    x = mergeTrees(node1, node4)
+    display(x)
